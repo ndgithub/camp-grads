439d5e25027178e2a6c1958b011ccd734467663e
ndgithub <nickydesai.mail@gmail.com>
2019-08-19 16:27:00 -0500

    Initial Commit, installed dependencies and setup basic server

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => res.send('App is running'));
+
+
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
