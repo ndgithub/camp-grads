302a9172e45a1e30469d5b97909223671edf396f
ndgithub <nickydesai.mail@gmail.com>
2019-08-20 09:44:51 -0500

    Add routes using express

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const connectDB = require('./config/db')
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Connect to db
+connectDB();
+
 app.get('/', (req, res) => res.send('App is running'));
 
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
