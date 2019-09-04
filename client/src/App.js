26275dc96def35806c1d9b589663c21f723dae48
ndgithub <nickydesai.mail@gmail.com>
2019-09-03 22:08:03 -0500

    Added client react folder. Added run scripts with concurrently package. Created landing page and navbar react components

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+import Navbar from './components/layout/Navbar.js';
+import Landing from './components/layout/Landing.js';
+
+
+import './App.css';
+
+const App = () =>
+  <Fragment>
+    <Navbar />
+    <Landing />
+
+  </Fragment>
+
+
+export default App;
